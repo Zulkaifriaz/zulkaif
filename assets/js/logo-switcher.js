513afb6b79ea41b9ee61437eb79aed67b8464b73
Zulkaif Riaz <90593596+Zulkaifriaz@users.noreply.github.com>
2025-12-31 17:07:59 +0500

    Change logo source for light mode

--- assets/js/logo-switcher.js
+++ assets/js/logo-switcher.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', function() {
         if (siteLogo) {
             siteLogo.src = isDark 
                 ? 'assets/images/logo/logo-light.png' 
-                : 'assets/images/logo/logox.jpg';
+                : 'assets/images/logo/logo-all-dark.png';
         }
     }
 
